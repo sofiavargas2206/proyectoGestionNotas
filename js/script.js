c285a959a0631321c374621c6d1b9b2c50360f87
cfpovedac90 <cfc_040@hotmail.com>
2022-10-05 20:24:24 -0500

    fondo html js prom

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ function myFunction() {
     document.getElementById('modalContainer').style.display='none';
 }
   
+function Promedio(){
+
+  var parcial,final,promedio;
+	parcial = parseFloat(promtp("Parcial"));
+	final = parseFloat(promtp("Final"));
+	promedio = (parcial+final)/2;
+
+	if(promedio >= 50.5){
+		document.write("El promedio es "+promedio+" APROBADO");
+	}else{
+		document.write("El promedio es "+promedio+" DESAPROBADO");
+	}
+
+}
