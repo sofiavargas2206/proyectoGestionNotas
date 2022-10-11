610b43b1f2b539acfcea347d8dad36ab9849b8b2
Nicoll Sofia Vargas Rojas <alejandro.jimenez1299@gmail.com>
2022-10-10 23:58:04 -0500

    CRUD estudiante lista

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from ast import Not
+from asyncio.windows_events import NULL
+from operator import not_
 from flask import Flask,render_template,request,redirect,url_for,flash
 from datetime import datetime
 import sqlite3 as sql
@@ -15,13 +18,30 @@ def Inicio():
 
 @app.route('/Estudiante')
 def Estudiante():
+   
     con=sql.connect('BaseNotas.db')
     con.row_factory=sql.Row
     cur=con.cursor()
-    cur.execute('select * from persona')
+    cur.execute('select * from persona where estado="Activo"')
     data=cur.fetchall()
     return render_template('Estudiante.html',datas=data)
 
+@app.route('/consultaEstudiante',methods=['GET','POST'])
+def consultaEstudiante():      
+    if request.method == 'POST':
+        documentoFiltro=request.form['documentoFiltro']
+        nombres=request.form['nombresFiltro']
+        con=sql.connect('BaseNotas.db')
+        con.row_factory=sql.Row
+        cur=con.cursor()
+        if documentoFiltro !='' or nombres!='':
+            cur.execute('select * from persona WHERE identificacion=? or nombres=? and estado="Activo"',(documentoFiltro,nombres) )
+           
+        else:
+            cur.execute('select * from persona')  
+           
+        data=cur.fetchall()
+    return render_template('Estudiante.html',datas=data)
 
 @app.route('/Docente')
 def Docente():
@@ -53,13 +73,53 @@ def addPersona():
         rol=request.form['rolPersona']
         fechaRegistro= datetime.now()
         fechaActualizacion= datetime.now()
+        estado='Activo'
         con=sql.connect('BaseNotas.db')
         cur=con.cursor()
-        cur.execute('insert into persona (nombres,apellidos,tipoIdentificacion,identificacion,telefono,email,fechaRegistro,fechaActualizacion,idRol) values (?,?,?,?,?,?,?,?,?)',(nombres,apellidos,tipoDocumento,documento,telefono,correo,fechaRegistro,fechaActualizacion,rol))
+        cur.execute('insert into persona (nombres,apellidos,tipoIdentificacion,identificacion,telefono,email,fechaRegistro,fechaActualizacion,idRol,estado) values (?,?,?,?,?,?,?,?,?,?)',(nombres,apellidos,tipoDocumento,documento,telefono,correo,fechaRegistro,fechaActualizacion,rol,estado))
         con.commit()
         flash('Usuario Guardado','success')
         return redirect(url_for('Estudiante'))
     return render_template('Estudiante.html')
+
+@app.route('/editPersona/<string:idPersona>',methods=['GET','POST'])
+def editPersona(idPersona):
+    if request.method == 'POST':
+        
+        con=sql.connect('BaseNotas.db')
+        con.row_factory=sql.Row
+        error=None
+        nombres=request.form['nombresPersona']
+        apellidos=request.form['apellidosPersona']
+        tipoDocumento=request.form['tipoDocumentoPersona']
+        documento=request.form['documentoPersona']
+        correo=request.form['correoPersona']
+        telefono=request.form['telefonoPersona']
+        cur=con.cursor()
+        cur.execute('update persona set nombres=?,apellidos=?,tipoIdentificacion=?,identificacion=?,telefono=?,email=? where idPersona=?',(nombres,apellidos,tipoDocumento,documento,telefono,correo,idPersona))
+        con.commit()
+        return redirect(url_for('Estudiante'))
+
+    con=sql.connect('BaseNotas.db')
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute('select * from persona where idPersona=?',(idPersona))
+    data=cur.fetchone()
+    return render_template('EstudianteActualizar.html',datas=data)
+
+    
+
+
+@app.route('/deletePersona/<string:idPersona>',methods=['GET'])
+def deletePersona(idPersona):
+    con=sql.connect('BaseNotas.db')
+    cur=con.cursor()
+    cur.execute('update persona set estado="Inactivo" where idPersona=?',(idPersona))
+    con.commit()
+    flash('Usuario Eliminado','warning')
+    return redirect(url_for('Estudiante'))
+
+
 
 
 if __name__=='__main__':
